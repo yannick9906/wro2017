6acb9a04223515111f59cff2703e4150ce52a611
Yannick Felix <yannick.felix1999@gmail.com>
2017-06-02 16:18:14 +0200

    erster Ast

--- src/Programm.py
+++ src/Programm.py
@@ -16,11 +16,11 @@ class Main(object):
         self.MMHeber = ev3.MediumMotor('outC')
         self.MMGreifer = ev3.MediumMotor('outA')
         self.MMGreifer.position = 0
-        self.MMGreifer.stop_action = 'hold'
-        self.MMGreifer.speed_sp = 300
+        #self.MMGreifer.stop_action = 'hold'
+        self.MMGreifer.speed_sp = 1500
         self.MMGreifer.ramp_up_sp = 300
         self.MMHeber.position = 0
-        self.MMHeber.stop_action = 'hold'
+        #self.MMHeber.stop_action = 'hold'
         self.MMHeber.speed_sp = 300
         self.MMGreifer.ramp_up_sp = 300
         # Sensoren:
@@ -50,13 +50,16 @@ class Main(object):
         self.greiferAuf()
         time.sleep(2)
         farbeGreifer = self.CSGreifer.color
-        while farbeGreifer == 0 or farbeGreifer == 5:
+        strecke = 0
+        while farbeGreifer == 0:
             self.LMLeft.run_timed(time_sp=100, speed_sp=-200)
             self.LMRight.run_timed(time_sp=100, speed_sp=-200)
+            strecke += 1
             farbeGreifer = self.CSGreifer.color
             print(farbeGreifer)
-        self.LMLeft.run_timed(time_sp=1, speed_sp=-200)
-        self.LMRight.run_timed(time_sp=1, speed_sp=-200)
+        self.LMLeft.run_timed(time_sp=300, speed_sp=-200)
+        self.LMRight.run_timed(time_sp=300, speed_sp=-200)
+        time.sleep(1)
         # AN GREIFER AUF ANPASSEN!!!
         Position = 0
         gedrückt = self.TSGreifer.is_pressed
@@ -64,13 +67,14 @@ class Main(object):
             self.MMGreifer.run_to_abs_pos(position_sp=Position)
             Position += 5
             gedrückt = self.TSGreifer.is_pressed
-        if Position >= 100:
-            Stein = "Mantel"
+        if strecke >= 50:
+            Stein = "POWER"
         else:
-            Stein = "Power"
+            Stein = "MANTEL"
         farbeStein = self.CSGreifer.color
         self.heberHoch()
-        return Stein, farbeStein
+        print(Stein, farbeStein, strecke)
+        return Stein, farbeStein, strecke
 
     def followBlackline(self):
         farbeLinks = self.CSLeft.color
@@ -120,56 +124,237 @@ class Main(object):
         self.LMLeft.run_timed(time_sp=2600, speed_sp=-300)
         self.LMRight.run_timed(time_sp=2600, speed_sp=-300)
         time.sleep(5)
-        self.LMLeft.run_timed(time_sp=1000, speed_sp=-200)
-        self.LMRight.run_timed(time_sp=1000, speed_sp=200)
+        self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+        self.LMRight.run_timed(time_sp=1100, speed_sp=200)
 
     def kleineBewegung(self):
         self.LMLeft.run_timed(time_sp=5000, speed_sp=-100)
         self.LMRight.run_timed(time_sp=5000, speed_sp=-100)
 
     def ersterZweig(self):
-        self.LMLeft.run_timed(time_sp=600, speed_sp=-200)
-        self.LMRight.run_timed(time_sp=600, speed_sp=200)
+        self.LMLeft.run_timed(time_sp=550, speed_sp=-200)
+        self.LMRight.run_timed(time_sp=550, speed_sp=200)
         time.sleep(2)
-        self.LMLeft.run_timed(time_sp=4300, speed_sp=-100)
-        self.LMRight.run_timed(time_sp=4300, speed_sp=-100)
+        self.LMLeft.run_timed(time_sp=4400, speed_sp=-100)
+        self.LMRight.run_timed(time_sp=4400, speed_sp=-100)
 
     def rechterZweig(self):
-        self.LMLeft.run_timed(time_sp=500, speed_sp=-200)
-        self.LMRight.run_timed(time_sp=500, speed_sp=200)
+        self.LMLeft.run_timed(time_sp=550, speed_sp=-200)
+        self.LMRight.run_timed(time_sp=550, speed_sp=200)
         time.sleep(2)
-        stein, farbeStein = self.steinUeberpruefen()
-        return stein, farbeStein
+        stein, farbeStein, strecke = self.steinUeberpruefen()
+        return stein, farbeStein, strecke
 
     def linkerZweig(self):
         for i in range(4):
             self.LMLeft.run_timed(time_sp=1000, speed_sp=100)
             self.LMRight.run_timed(time_sp=1000, speed_sp=100)
         time.sleep(2)
-        self.LMLeft.run_timed(time_sp=850, speed_sp=200)
-        self.LMRight.run_timed(time_sp=850, speed_sp=-200)
-        stein, farbeStein = self.steinUeberpruefen()
-        return stein, farbeStein
+        self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+        self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+        stein, farbeStein, strecke = self.steinUeberpruefen()
+        return stein, farbeStein, strecke
 
+    def ersterZweigRueckweg(self, rechts, strecke):
+        weg = strecke * 100
+        self.LMLeft.run_timed(time_sp=weg, speed_sp=200)
+        self.LMRight.run_timed(time_sp=weg, speed_sp=200)
+        time.sleep(5)
+        if rechts == True:
+             pass
+        else:
+             self.LMLeft.run_timed(time_sp=1000, speed_sp=-200)
+             self.LMRight.run_timed(time_sp=1000, speed_sp=200)
+        time.sleep(1)
+        self.LMLeft.run_timed(time_sp=550, speed_sp=200)
+        self.LMRight.run_timed(time_sp=550, speed_sp=-200)
+        time.sleep(1)
+        # An der Gabelung
+        self.LMLeft.run_timed(time_sp=4400, speed_sp=100)
+        self.LMRight.run_timed(time_sp=4400, speed_sp=100)
+        time.sleep(5)
+        self.LMLeft.run_timed(time_sp=550, speed_sp=200)
+        self.LMRight.run_timed(time_sp=550, speed_sp=-200)
+        time.sleep(1)
+        self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+        self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+
+    def FollowBlackLineBack(self):
+        farbeLinks = self.CSLeft.color
+        farbeRechts = self.CSRight.color
+        while not(farbeLinks == 1 and farbeRechts == 1):
+            if farbeLinks == 6 and farbeRechts == 6:
+               self.LMLeft.run_timed(time_sp=150, speed_sp=200)
+               self.LMRight.run_timed(time_sp=150, speed_sp=200)
+               farbeLinks = self.CSLeft.color
+               farbeRechts = self.CSRight.color
+               print(farbeLinks, farbeRechts)
+            elif farbeLinks == 1 and farbeRechts == 6:
+               self.LMLeft.run_timed(time_sp=150, speed_sp=200)
+               time.sleep(0.1)
+               self.LMLeft.run_timed(time_sp=150, speed_sp=200)
+               self.LMRight.run_timed(time_sp=150, speed_sp=200)
+               time.sleep(0.1)
+               self.LMRight.run_timed(time_sp=150, speed_sp=200)
+               farbeLinks = self.CSLeft.color
+               farbeRechts = self.CSRight.color
+               print(farbeLinks, farbeRechts)
+            elif farbeLinks == 6 and farbeRechts == 1:
+               self.LMRight.run_timed(time_sp=150, speed_sp=200)
+               time.sleep(0.1)
+               self.LMLeft.run_timed(time_sp=150, speed_sp=200)
+               self.LMRight.run_timed(time_sp=150, speed_sp=200)
+               time.sleep(0.1)
+               self.LMLeft.run_timed(time_sp=150, speed_sp=200)
+               farbeLinks = self.CSLeft.color
+               farbeRechts = self.CSRight.color
+               print(farbeLinks, farbeRechts)
+            else:
+               farbeLinks = self.CSLeft.color
+               farbeRechts = self.CSRight.color
+               print("ERROR!!!")
+        print("fertig")
+        time.sleep(1)
+
+    def ZielPlatzieren(self):
+        farbeStein = self.CSGreifer.color
+        if farbeStein == 2:
+            self.LMLeft.run_timed(time_sp=550, speed_sp=200)
+            self.LMRight.run_timed(time_sp=550, speed_sp=-200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=200)
+            time.sleep(1.5)
+            self.LMLeft.run_timed(time_sp=9000, speed_sp=100)
+            self.LMRight.run_timed(time_sp=9000, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=200)
+            time.sleep(1)
+        elif farbeStein == 3:
+            self.LMLeft.run_timed(time_sp=1650, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1650, speed_sp=-200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(1)
+        elif farbeStein == 4:
+            self.LMLeft.run_timed(time_sp=550, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=550, speed_sp=200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+            time.sleep(1.5)
+            self.LMLeft.run_timed(time_sp=9000, speed_sp=100)
+            self.LMRight.run_timed(time_sp=9000, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+            time.sleep(1)
+        elif farbeStein == 5:
+            self.LMLeft.run_timed(time_sp=550, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=550, speed_sp=200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+            time.sleep(1.5)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+            time.sleep(1)
+        self.heberHoch()
+        self.LMLeft.run_timed(time_sp=500, speed_sp=100)
+        self.LMRight.run_timed(time_sp=500, speed_sp=100)
+        return farbeStein
+
+    def Zielzurueck(self, farbeStein):
+        if farbeStein == 2:
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=9000, speed_sp=100)
+            self.LMRight.run_timed(time_sp=9000, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
+            time.sleep(1.5)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=550, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=550, speed_sp=200)
+            time.sleep(1)
+        if farbeStein == 3:
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=1650, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1650, speed_sp=200)
+            time.sleep(1)
+        if farbeStein == 4:
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=9000, speed_sp=100)
+            self.LMRight.run_timed(time_sp=9000, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=200)
+            time.sleep(1.5)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=550, speed_sp=200)
+            self.LMRight.run_timed(time_sp=550, speed_sp=-200)
+            time.sleep(1)
+        if farbeStein == 5:
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=200)
+            time.sleep(1)
+            self.LMLeft.run_timed(time_sp=4500, speed_sp=100)
+            self.LMRight.run_timed(time_sp=4500, speed_sp=100)
+            time.sleep(5)
+            self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=1100, speed_sp=200)
+            time.sleep(1.5)
     #
 #####################################################################################
 # Hauptprogramm:
 #####################################################################################
 
-    def Programm(self):
+    def programm(self):
         self.Beginn()
         time.sleep(5)
         self.kleineBewegung()
         time.sleep(2.5)
         self.ersterZweig()
         time.sleep(5)
-        stein, farbeStein = self.rechterZweig()
+        strecke = 0
+        stein, farbeStein, strecke = self.rechterZweig()
         time.sleep(5)
-        if stein == Mantel:
+        rechts = True
+        if stein == "MANTEL":
             pass
         else:
-            stein, farbeStein = self.linkerZweig()
+            stein, farbeStein, strecke = self.linkerZweig()
+            rechts = False
+        self.ersterZweigRueckweg(rechts, strecke)
+        farbeStein = self.ZielPlatzieren()
+        self.Zielzurueck(farbeStein)
 
-# Einrichtung des Roboters:
+
+#Einrichtung des Roboters:
 m = Main()
 print('Die kurfuerstlichen Roboter')
+m.programm()
