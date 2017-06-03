973e0aa64f9363a71e17490304f8f126d0d6fac0
Yannick Felix <yannick.felix1999@gmail.com>
2017-06-03 15:26:44 +0200

    erster Ast

--- src/programmsemi.py
+++ src/programmsemi.py
@@ -34,18 +34,33 @@ class Main(object):
         self.LMRight.stop()
         self.LMLeft.stop()
 
+    def logToFile(self, text):
+        print(text)
+        with open("logfile.log", 'a') as file:
+            file.write("\n"+text)
+
     def readOutSensors(self):
-        print('TSGreifer:',self.TSGreifer.is_pressed,'CSGreifer:',self.CSGreifer.color,'CSLeft:',self.CSLeft.color,'CSRight:',self.CSRight.color)
+        self.logToFile('TSGreifer: '+str(self.TSGreifer.is_pressed)+' CSGreifer: '+str(self.CSGreifer.color)+' CSLeft: '+str(self.CSLeft.color)+' CSRight: '+str(self.CSRight.color))
 
     def rotate90DegLeft(self):
         self.LMLeft.run_timed(time_sp=1100)
-        self.LMRight.run_timed(time_sp=1100,speed_sp=-200)
+        self.LMRight.run_timed(time_sp=1100, speed_sp=-200)
         time.sleep(1.1)
 
     def rotate90DegRight(self):
         self.LMRight.run_timed(time_sp=1100)
-        self.LMLeft.run_timed(time_sp=1100,speed_sp=-200)
+        self.LMLeft.run_timed(time_sp=1100, speed_sp=-200)
         time.sleep(1.1)
+
+    def rotate45DegLeft(self):
+        self.LMRight.run_timed(time_sp=550, speed_sp=-200)
+        self.LMLeft.run_timed(time_sp=550)
+        time.sleep(.6)
+
+    def rotate45DegRight(self):
+        self.LMRight.run_timed(time_sp=550)
+        self.LMLeft.run_timed(time_sp=550, speed_sp=-200)
+        time.sleep(.6)
 
     def runToGreenEnd(self):
         self.LMLeft.run_forever()
@@ -56,7 +71,7 @@ class Main(object):
             green = (self.CSLeft.color==3 or self.CSLeft.color==2) and (self.CSRight.color==3 or self.CSRight.color==2)
         self.LMRight.stop()
         self.LMLeft.stop()
-        print("Green end reached.")
+        self.logToFile("Green end reached.")
 
     def runToFirstJunctionFromGreen(self):
         self.runToGreenEnd()
@@ -70,18 +85,97 @@ class Main(object):
             self.LMRight.run_forever()
             if self.CSLeft.color==1 and self.CSRight!=1:
                 self.LMLeft.stop()
-                print("Left")
+                self.logToFile("Left")
             elif self.CSLeft.color!=1 and self.CSRight==1:
                 self.LMRight.stop()
-                print("Right")
-        print("Junction reached.")
+                self.logToFile("Right")
+        self.logToFile("Junction reached.")
         self.LMLeft.stop()
         self.LMRight.stop()
+
+    def greiferAuf(self):
+        self.MMGreifer.run_to_abs_pos(position_sp=-330)
+
+    def heberHoch(self):
+        self.MMHeber.run_to_abs_pos(position_sp=0)
+
+    def heberRunter(self):
+        self.MMHeber.run_to_abs_pos(position_sp=-440)
+
+    def steinUeberpruefen(self):
+        self.heberHoch()
+        self.heberRunter()
+        self.greiferAuf()
+        time.sleep(2)
+        farbeGreifer = self.CSGreifer.color
+        strecke = 0
+        while farbeGreifer == 0:
+            self.LMLeft.run_timed(time_sp=100, speed_sp=-200)
+            self.LMRight.run_timed(time_sp=100, speed_sp=-200)
+            strecke += 1
+            farbeGreifer = self.CSGreifer.color
+            print(farbeGreifer)
+        self.LMLeft.run_timed(time_sp=300, speed_sp=-200)
+        self.LMRight.run_timed(time_sp=300, speed_sp=-200)
+        time.sleep(1)
+        # AN GREIFER AUF ANPASSEN!!!
+        Position = 0
+        gedrückt = self.TSGreifer.is_pressed
+        while gedrückt != 1:
+            self.MMGreifer.run_to_abs_pos(position_sp=Position)
+            Position += 5
+            gedrückt = self.TSGreifer.is_pressed
+        if strecke >= 50:
+            Stein = "POWER"
+        else:
+            Stein = "MANTEL"
+        farbeStein = self.CSGreifer.color
+        self.heberHoch()
+        self.logToFile(str(Stein)+" "+str(farbeStein)+" "+str(strecke))
+        return Stein, farbeStein, strecke
+
+    def firstbranch1(self):
+        # Erste Ast
+        self.rotate45DegLeft()
+        self.runToNextJunction()
+        self.rotate45DegLeft()
+        self.rotate90DegLeft()
+        self.rotate90DegLeft()
+
+    def firstbranch1back(self):
+        self.runToNextJunction()
+        self.rotate45DegRight()
+        self.runToNextJunction()
+        self.rotate45DegRight()
+
+    def runToFundament(self, color):
+        self.runToNextJunction()
+        self.runToNextJunction()
+        if color == 1:
+            pass
+        elif color == 2:
+            pass
+        elif color == 3:
+            pass
+        elif color == 4:
+            pass
+        elif color == 5:
+            pass
 
     def run(self):
         self.runToFirstJunctionFromGreen()
         self.rotate90DegRight()
         self.runToNextJunction()
+
+        self.firstbranch1()
+
+        stein, farbe, strecke = self.steinUeberpruefen()
+        if stein == "MANTEL":
+            self.firstbranch1back()
+            self.runToFundament(farbe)
+        elif stein == "POWER":
+            pass  # Todo fallen lassen & Farbe merken
+
 
 m = Main()
 m.run()
